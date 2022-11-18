ffde3dfca2c18083e1d24e3aa73f7e213483d82e
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2022-11-18 12:43:43 -0500

    initizing git, simple form

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+
+
+
+app.get('/', function (req, res)  {
+    res.render("landingpage.ejs")
+    
+})
+
+app.listen(2000, function() {
+    console.log("server has began")
+  });
+  
